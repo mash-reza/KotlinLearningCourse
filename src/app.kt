7e8c8a1491cbe172f395b542eab0486969a28775
Festive <27864684+mash-reza@users.noreply.github.com>
2019-08-17 21:02:56 +0430

    using safe ops (?.) when the object is nullable, better safe than sorry huh?!

--- src/app.kt
+++ src/app.kt
@@ -1,8 +1,6 @@
 fun main() {
-    var num1 : Int = 5
-    var num2 : Int = 8
 
-    var result : Int = if (num1 > num2) num1 else num2
-    println(result)
+    var instance: Human? = Human()
+    println(instance?.name?.length)
 
 }
